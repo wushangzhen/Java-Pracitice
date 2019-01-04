06b62010f5c8c15193f1e91b98d877c87b777168
wushangzhen <wushangzhen_bupt@163.com>
2019-01-03 23:43:33 -0800

    1-3-2019

---
+++ design_pattern/Strategy_Pattern/BubbleSorter.java
@@ -0,0 +1,34 @@
+import java.util.Comparator;
+
+public class BubbleSorter implements Sorter {
+    @Override
+    public <T extends Comparable<T>> void sort(T[] list) {
+        boolean swapped = true;
+        for (int i = 1, len = list.length; i < len && swapped; ++i) {
+            swapped = false;
+            for (int j = 0; j < len - i; j++) {
+                if (list[j].compareTo(list[j + 1]) > 0) {
+                    T temp = list[j];
+                    list[j] = list[j + 1];
+                    list[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+        }
+    }
+    @Override
+    public <T> void sort(T[] list, Comparator<T> comp) {
+        boolean swapped = true;
+        for (int i = 1, len = list.length; i < len && swapped; ++i) {
+            swapped = true;
+            for (int j = 0; j < len - i; ++j) {
+                if (comp.compare(list[j], list[j + 1]) > 0) {
+                    T temp = list[j];
+                    list[j] = list[j + 1];
+                    list[j + 1] = temp;
+                    swapped = false;
+                }
+            } 
+        }
+    }
+}
